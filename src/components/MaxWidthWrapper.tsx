611b2e8e671cc2d8dc8c3e3100c0ac80a48806ab
Pedro Souza <pedrops244@hotmail.com>
2024-05-16 20:16:01 -0300

    components to landing page

--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -1,5 +1,5 @@
-import { cn } from "@/lib/utils";
-import { ReactNode } from "react";
+import { cn } from '@/lib/utils';
+import { ReactNode } from 'react';
 
 const MaxWidthWrapper = ({
   className,
@@ -11,7 +11,7 @@ const MaxWidthWrapper = ({
   return (
     <div
       className={cn(
-        "h-full mx-auto w-full max-w-screen-xl px-2.5 md:px-20",
+        'h-full mx-auto w-full max-w-screen-xl px-2.5 md:px-20',
         className,
       )}
     >

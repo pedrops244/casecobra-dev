71a4cac32e9507310c8432f912ebf2ff7b281194
Pedro Souza <pedrops244@hotmail.com>
2024-05-23 12:17:22 -0300

    creating a uploading page and the logic behind that

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import NavBar from '@/components/NavBar';
 import Footer from '@/components/Footer';
+import { Toaster } from '@/components/ui/toaster';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,8 +21,12 @@ export default function RootLayout({
     <html lang='pt-BR'>
       <body className={inter.className}>
         <NavBar />
-        {children}
-        <Footer />
+
+        <main className='flex flex-col min-h-[calc(100vh-3.5rem-1px)]'>
+          <div className='flex-1 flex flex-col h-full'>{children}</div>
+          <Footer />
+        </main>
+        <Toaster />
       </body>
     </html>
   );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,14 +5,14 @@ const Footer = () => {
   return (
     <footer className='bg-white h-20 relative'>
       <MaxWidthWrapper>
-        <div className='broder-t border-gray-200' />
-        <div className='h-full flex flex-col md:flex-row mg:justify-between justify-center items-center'>
+        <div className='border-t border-gray-200' />
+        <div className='h-full flex flex-col md:flex-row md:justify-between justify-center items-center'>
           <div className='text-center md:text-left pb-2 md:pb-0'>
             <p className='text-sm text-muted-foreground'>
-              {' '}
               &copy; {new Date().getFullYear()} All rights reserved{' '}
             </p>
           </div>
+
           <div className='flex items-center justify-center'>
             <div className='flex space-x-8'>
               <Link

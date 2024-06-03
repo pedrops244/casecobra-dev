c00601af89c17208945d874e25a2e89e90921bdf
Pedro Souza <pedrops244@hotmail.com>
2024-06-03 11:19:43 -0300

    finishing the "customize your case" page, but isn't resposive

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -1,13 +1,18 @@
 'use client';
 import HandleComponent from '@/components/HandleComponent';
 import { ScrollArea } from '@/components/ui/scroll-area';
-import { cn } from '@/lib/utils';
+import { cn, formatPrice } from '@/lib/utils';
 import { AspectRatio } from '@radix-ui/react-aspect-ratio';
 import NextImage from 'next/image';
 import { Rnd } from 'react-rnd';
 import { RadioGroup } from '@headlessui/react';
 import { useState } from 'react';
-import { COLORS, MODELS } from '@/validators/option-validator';
+import {
+  COLORS,
+  FINISHES,
+  MODELS,
+  MATERIALS,
+} from '@/validators/option-validator';
 import { Label } from '@/components/ui/label';
 import {
   DropdownMenu,
@@ -16,7 +21,8 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { Button } from '@/components/ui/button';
-import { Check, ChevronsUpDown } from 'lucide-react';
+import { ArrowRight, Check, ChevronsUpDown } from 'lucide-react';
+import { BASE_PRICE } from '@/config/products';
 
 interface DesignConfiguratorProps {
   configId: string;
@@ -32,9 +38,13 @@ const DesignConfigurator = ({
   const [options, setOptions] = useState<{
     color: (typeof COLORS)[number];
     model: (typeof MODELS.options)[number];
+    material: (typeof MATERIALS.options)[number];
+    finish: (typeof FINISHES.options)[number];
   }>({
     color: COLORS[0],
     model: MODELS.options[0],
+    material: MATERIALS.options[0],
+    finish: FINISHES.options[0],
   });
 
   return (
@@ -175,10 +185,91 @@ const DesignConfigurator = ({
                     </DropdownMenuContent>
                   </DropdownMenu>
                 </div>
+                {[MATERIALS, FINISHES].map(
+                  ({ name, options: selectableOptions }) => (
+                    <RadioGroup
+                      key={name}
+                      value={options[name]}
+                      onChange={(value) => {
+                        setOptions((prev) => ({
+                          ...prev,
+                          [name]: value,
+                        }));
+                      }}
+                    >
+                      <Label>
+                        {name.slice(0, 1).toUpperCase() + name.slice(1)}
+                      </Label>
+                      <div className='mt-3 space-y-4'>
+                        {selectableOptions.map((option) => (
+                          <RadioGroup.Option
+                            key={option.value}
+                            value={option}
+                            className={({ active, checked }) =>
+                              cn(
+                                'relative block cursor-pointer rounded-lg bg-white px-6 py-4 shadow-sm border-2 border-zinc-200 focus:outline:-none ring-0 focus:ring-0 outline-none sm:flex sm:justify-between',
+                                {
+                                  'border-primary': active || checked,
+                                },
+                              )
+                            }
+                          >
+                            <span className='flex items-center'>
+                              <span className='flex flex-col text-sm'>
+                                <RadioGroup.Label
+                                  className='font-medium text-gray-900'
+                                  as='span'
+                                >
+                                  {option.label}
+                                </RadioGroup.Label>
+                                {option.description ? (
+                                  <RadioGroup.Description
+                                    as='span'
+                                    className='text-gray-500'
+                                  >
+                                    <span className='block sm:inline'>
+                                      {option.description}
+                                    </span>
+                                  </RadioGroup.Description>
+                                ) : null}
+                              </span>
+                            </span>
+                            <RadioGroup.Description
+                              as='span'
+                              className='mt-2 flex text-sm sm:ml-4 sm:mt-0 sm:flex-col sm:text-right'
+                            >
+                              <span className='font-medium text-gray-900'>
+                                {formatPrice(option.price / 100)}
+                              </span>
+                            </RadioGroup.Description>
+                          </RadioGroup.Option>
+                        ))}
+                      </div>
+                    </RadioGroup>
+                  ),
+                )}
               </div>
             </div>
           </div>
         </ScrollArea>
+
+        <div className='w-full px-8 h-16 bg-white'>
+          <div className='h-px w-full bg-zinc-200' />
+          <div className='w-full h-full flex justify-end items-center'>
+            <div className='w-full flex gap-6 items-center'>
+              <p className='font-medium whitespace-nowrap'>
+                {formatPrice(
+                  (BASE_PRICE + options.finish.price + options.material.price) /
+                    100,
+                )}
+              </p>
+              <Button size='sm' className='w-full'>
+                Continue
+                <ArrowRight className='h-4 w-4 ml-1.5 inline' />
+              </Button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/validators/option-validator.ts
+++ src/validators/option-validator.ts
@@ -2,6 +2,8 @@
 // bg-zinc-950 border-zinc-950
 // bg-rose-950 border-rose-950
 
+import { PRODUCT_PRICES } from '@/config/products';
+
 export const COLORS = [
   { label: 'Black', value: 'black', tw: 'zinc-950' },
   { label: 'Blue', value: 'blue', tw: 'blue-950' },
@@ -34,6 +36,42 @@ export const MODELS = {
     {
       label: 'iPhone 15',
       value: 'iphone15',
+    },
+  ],
+} as const;
+
+export const MATERIALS = {
+  name: 'material',
+  options: [
+    {
+      label: 'Silicone',
+      value: 'silicone',
+      description: undefined,
+      price: PRODUCT_PRICES.material.silicone,
+    },
+    {
+      label: 'Soft Polycarbonate',
+      value: 'polycarbonate',
+      description: 'Scratch-resistant coating',
+      price: PRODUCT_PRICES.material.polycarbonate,
+    },
+  ],
+} as const;
+
+export const FINISHES = {
+  name: 'finish',
+  options: [
+    {
+      label: 'Smooth Finish',
+      value: 'smooth',
+      description: undefined,
+      price: PRODUCT_PRICES.finish.smooth,
+    },
+    {
+      label: 'Textured',
+      value: 'textured',
+      description: 'Soft grippy texture',
+      price: PRODUCT_PRICES.finish.textured,
     },
   ],
 } as const;

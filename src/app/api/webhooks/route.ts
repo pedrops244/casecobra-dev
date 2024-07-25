7c25c9b207b96cc821a392958db1b4dbadffea65
Pedro Souza <pedrops244@hotmail.com>
2024-07-25 14:17:34 -0300

    fix cors

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -8,48 +8,27 @@ import OrderReceivedEmail from '@/components/emails/OrderReceivedEmail';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-const corsHeaders = {
-  'Access-Control-Allow-Origin': `${process.env.NEXT_PUBLIC_SERVER_URL}`,
-  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'Access-Control-Allow-Headers':
-    'Content-Type, Authorization, stripe-signature',
-};
-
 export async function POST(req: Request) {
   try {
     const body = await req.text();
     const signature = headers().get('stripe-signature');
 
     if (!signature) {
-      console.error('Missing signature');
-      return new NextResponse('Invalid signature', {
-        status: 400,
-        headers: corsHeaders,
-      });
+      return new Response('Invalid signature', { status: 400 });
     }
 
-    let event: Stripe.Event;
-    try {
-      event = stripe.webhooks.constructEvent(
-        body,
-        signature,
-        process.env.STRIPE_WEBHOOK_SECRET!,
-      );
-    } catch (err) {
-      console.error('Error constructing Stripe event:', err);
-      return new NextResponse('Error constructing event', {
-        status: 400,
-        headers: corsHeaders,
-      });
-    }
+    const event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET!,
+    );
 
     if (event.type === 'checkout.session.completed') {
-      const session = event.data.object as Stripe.Checkout.Session;
-      const { customer_details } = session;
-
-      if (!customer_details?.email) {
+      if (!event.data.object.customer_details?.email) {
         throw new Error('Missing user email');
       }
+
+      const session = event.data.object as Stripe.Checkout.Session;
 
       const { userId, orderId } = session.metadata || {
         userId: null,
@@ -60,16 +39,18 @@ export async function POST(req: Request) {
         throw new Error('Invalid request metadata');
       }
 
-      const billingAddress = customer_details!.address;
+      const billingAddress = session.customer_details!.address;
       const shippingAddress = session.shipping_details!.address;
 
       const updatedOrder = await db.order.update({
-        where: { id: orderId },
+        where: {
+          id: orderId,
+        },
         data: {
           isPaid: true,
           shippingAddress: {
             create: {
-              name: customer_details!.name!,
+              name: session.customer_details!.name!,
               city: shippingAddress!.city!,
               country: shippingAddress!.country!,
               postalCode: shippingAddress!.postal_code!,
@@ -79,7 +60,7 @@ export async function POST(req: Request) {
           },
           billingAddress: {
             create: {
-              name: customer_details!.name!,
+              name: session.customer_details!.name!,
               city: billingAddress!.city!,
               country: billingAddress!.country!,
               postalCode: billingAddress!.postal_code!,
@@ -92,14 +73,14 @@ export async function POST(req: Request) {
 
       await resend.emails.send({
         from: 'CaseCobra <pedrops244@hotmail.com>',
-        to: [customer_details.email],
+        to: [event.data.object.customer_details.email],
         subject: 'Thanks for your order!',
         react: OrderReceivedEmail({
           orderId,
           orderDate: updatedOrder.createdAt.toLocaleDateString(),
           // @ts-ignore
           shippingAddress: {
-            name: customer_details!.name!,
+            name: session.customer_details!.name!,
             city: shippingAddress!.city!,
             country: shippingAddress!.country!,
             postalCode: shippingAddress!.postal_code!,
@@ -110,26 +91,13 @@ export async function POST(req: Request) {
       });
     }
 
-    return new NextResponse(JSON.stringify({ result: event, ok: true }), {
-      headers: corsHeaders,
-    });
+    return NextResponse.json({ result: event, ok: true });
   } catch (err) {
-    console.error('Error processing webhook:', err);
+    console.error(err);
 
-    return new NextResponse(
-      JSON.stringify({ message: 'Something went wrong', ok: false }),
-      {
-        status: 500,
-        headers: corsHeaders,
-      },
+    return NextResponse.json(
+      { message: 'Something went wrong', ok: false },
+      { status: 500 },
     );
   }
-}
-
-// Função para lidar com requisições OPTIONS para CORS
-export function OPTIONS() {
-  return new NextResponse(null, {
-    status: 204,
-    headers: corsHeaders,
-  });
 }
